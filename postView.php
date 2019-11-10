af47890c7445c8f042ec25aa430ca423327bd4b8
Esteban Vignon <vignon.esteban@outlook.com>
2019-11-10 09:36:23 +0100

    Create Page Template for Blog Page & Posts

--- postView.php
+++ postView.php
@@ -1,160 +1,64 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php $title = 'CV Esteban Vignon - ' . $post['title']; ?>
+<?php $description = 'Article de blog : ' . $post['title'];; ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Esteban Vignon Freelance PHP Symphony">
+<?php ob_start(); ?>
 
-    <title>Esteban Vignon Freelance PHP Symphony</title>
-
-    <!-- Custom fonts for this theme -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- Theme CSS -->
-    <link href="css/style.css" rel="stylesheet">
-
-</head>
-
-<body id="page-top">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="index.html">Accueil</a>
-            <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item mx-0 mx-lg-1">
-                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="blog.php">Blog</a>
-                    </li>
-                    <li class="nav-item mx-0 mx-lg-1">
-                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.html#about">À
-                            Propos</a>
-                    </li>
-                    <li class="nav-item mx-0 mx-lg-1">
-                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.html#contact">Contact</a>
-                    </li>
-                </ul>
+<!-- Masthead -->
+<header class="bg-primary text-white text-center masthead-blog">
+    <div class="container d-flex align-items-center flex-column">
+        <div class="row col-12">
+            <div class="col-12">
+                <h1 class="text-body"><?= $post['title']; ?></h1>
+                <p class="text-black-50 chapo"><?= $post['heading']; ?></p>
             </div>
         </div>
-    </nav>
 
-    <!-- Masthead -->
-    <header class="bg-primary text-white text-center masthead-blog">
-        <div class="container d-flex align-items-center flex-column">
-            <div class="row col-12">
-                <div class="col-12">
-                    <h1 class="text-body"><?= $post['title']; ?></h1>
-                    <p class="text-black-50 chapo"><?= $post['heading']; ?></p>
+        <div class="row">
+            <div class="mt-4 pl-4 pr-4 col-md-10 offset-md-1">
+
+                <p class="text-body text-left"><?= $post['content']; ?></p>
+
+                <p class="text-left text-body mt-5"><b>Rédigé par : </b><?= $author['username'] ?></p>
+                <p class="text-left text-body"><b>Le :</b> <?= date('d/m/Y', strtotime($post['created_at'])) ?></p>
+            </div>
+        </div>
+
+        <div class="row col-12">
+
+            <div class="mt-5 pl-4 pr-4 col-12">
+                <h3 class="text-body">Commentaires</h3>
+            </div>
+
+
+            <?php foreach ($comments as $comment) : ?>
+                <div class="comment-post p-3 text-white col-12 mt-3 offset-md-1 col-md-8">
+                    <p class="text-left text-body"><b><?= 'Par ' . $comment['firstname'] . ' ' . $comment['lastname'] . ' le ' . date('d/m/Y à g:i', strtotime($comment['created_at'])) ?></b></p>
+                    <p class="text-left text-body">Super article de blog ! Continuez comme ça ! :)</p>
                 </div>
-            </div>
+            <?php endforeach ?>
 
-            <div class="row">
-                <div class="mt-4 pl-4 pr-4 col-md-10 offset-md-1">
-
-                    <p class="text-body text-left"><?= $post['content']; ?></p>
-
-                    <p class="text-left text-body mt-5"><b>Rédigé par : </b><?= $author['username'] ?></p>
-                    <p class="text-left text-body"><b>Le :</b> <?= date('d/m/Y', strtotime($post['created_at'])) ?></p>
-                </div>
-            </div>
-
-            <div class="row col-12">
-
-                <div class="mt-5 pl-4 pr-4 col-12">
-                    <h3 class="text-body">Commentaires</h3>
-                </div>
-
-
-                <?php foreach ($comments as $comment) : ?>
-                    <div class="comment-post p-3 text-white col-12 mt-3 offset-md-1 col-md-8">
-                        <p class="text-left text-body"><b><?= 'Par ' . $comment['firstname'] . ' ' . $comment['lastname'] . ' le ' . date('d/m/Y à g:i', strtotime($comment['created_at'])) ?></b></p>
-                        <p class="text-left text-body">Super article de blog ! Continuez comme ça ! :)</p>
-                    </div>
-                <?php endforeach ?>
-
-
-            </div>
-
-            <div class="row mt-5">
-                <form role="form" class="col-12">
-                    <div class="form-group text-black-50">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1" class="text-black-50">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
 
         </div>
 
-    </header>
-
-    <!-- Footer -->
-    <footer class="footer text-center">
-        <div class="container">
-            <div class="row">
-
-                <!-- Footer Location -->
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Emplacement</h4>
-                    <p class="lead mb-0">Lyon 5ème
-                        <br>France</p>
+        <div class="row mt-5">
+            <form role="form" class="col-12">
+                <div class="form-group text-black-50">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1" class="text-black-50">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
 
-                <!-- Footer Social Icons -->
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Mes Reseaux</h4>
-                    <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/EstebanVignon" target="_blank">
-                        <i class="fab fa-github"></i>
-                    </a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/estebanvignon/" target="_blank">
-                        <i class="fab fa-fw fa-linkedin-in"></i>
-                    </a>
-                </div>
-
-            </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-    </footer>
 
-    <!-- Copyright Section -->
-    <section class="copyright py-4 text-center text-white">
-        <div class="container">
-            <small>Copyright &copy; Esteban Vignon - 2019</small>
-        </div>
-    </section>
-
-    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-    <div class="scroll-to-top d-lg-none position-fixed ">
-        <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
-            <i class="fa fa-chevron-up"></i>
-        </a>
     </div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</header>
 
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<?php $content = ob_get_clean(); ?>
 
-    <!-- Contact Form JavaScript -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="js/freelancer.js"></script>
-
-</body>
-
-</html>
+<?php require('template.php'); ?>
