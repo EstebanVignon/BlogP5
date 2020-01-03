e00b4c1fadc73b6138614f695b1d9e168881fe86
Esteban Vignon <vignon.esteban@outlook.com>
2020-01-03 16:00:38 +0100

    Secure CRUD comment account blog_post #2

--- controller/Dashboard.php
+++ controller/Dashboard.php
@@ -4,11 +4,6 @@ class Dashboard extends Controller
 {
     public function showDashboard($request)
     {
-        if ($this->sessionManager->get('role') === null) {
-            $this->redirect('login');
-            exit();
-        }
-
         $posts = null;
         $comments = null;
 
@@ -49,134 +44,104 @@ class Dashboard extends Controller
 
     public function addPost($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Admin') {
+            $manager = new PostManager();
+            $manager->create($request, $this->sessionManager->get('id'));
+            $this->redirect('dashboard');
         }
-
-        $manager = new PostManager();
-        $manager->create($request, $this->sessionManager->get('id'));
-        $this->redirect('dashboard');
     }
 
     public function deletePost($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new PostManager();
+            $post = $manager->find($request['id']);
+            $manager->delete($post);
+            $this->redirect('dashboard');
         }
-
-        $manager = new PostManager();
-        $post = $manager->find($request['id']);
-        $manager->delete($post);
-        $this->redirect('dashboard');
     }
 
     public function showEditPost($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $postManager = new PostManager();
+            $post = $postManager->find($request['id']);
+
+            $accountManager = new AccountManager();
+            $accounts = $accountManager->findOtherAdminAccounts($this->sessionManager->get('id'));
+
+            $this->render(
+                'dashboard/_editPost.php',
+                array('post' => $post, 'accounts' => $accounts),
+                'Edition de l\'article ' . $post->getTitle(),
+                'Contenu : ' . $post->getContent()
+            );
         }
-
-        $postManager = new PostManager();
-        $post = $postManager->find($request['id']);
-
-        $accountManager = new AccountManager();
-        $accounts = $accountManager->findOtherAdminAccounts($this->sessionManager->get('id'));
-
-        $this->render(
-            'dashboard/_editPost.php',
-            array('post' => $post, 'accounts' => $accounts),
-            'Edition de l\'article ' . $post->getTitle(),
-            'Contenu : ' . $post->getContent()
-        );
     }
 
     public function editPost($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new PostManager();
+            $manager->edit($request);
+            $this->redirect('dashboard');
         }
-
-        $manager = new PostManager();
-        $manager->edit($request);
-        $this->redirect('dashboard');
     }
 
     public function deleteComment($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new CommentManager();
+            $comment = $manager->find($request['id']);
+            $manager->delete($comment);
+            $this->redirect('dashboard');
         }
-
-        $manager = new CommentManager();
-        $comment = $manager->find($request['id']);
-        $manager->delete($comment);
-        $this->redirect('dashboard');
     }
 
     public function approveComment($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new CommentManager();
+            $manager->approve($request['id']);
+            $this->redirect('dashboard');
         }
-
-        $manager = new CommentManager();
-        $manager->approve($request['id']);
-        $this->redirect('dashboard');
     }
 
     public function disapproveComment($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new CommentManager();
+            $manager->disapprove($request['id']);
+            $this->redirect('dashboard');
         }
-
-        $manager = new CommentManager();
-        $manager->disapprove($request['id']);
-        $this->redirect('dashboard');
     }
 
     public function promoteAccount($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new AccountManager();
+            $manager->promote($request['id']);
+            $this->redirect('dashboard');
         }
-
-        $manager = new AccountManager();
-        $manager->promote($request['id']);
-        $this->redirect('dashboard');
     }
 
     public function decreaseAccount($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new AccountManager();
+            $manager->decrease($request['id']);
+            $this->redirect('dashboard');
         }
-
-        $manager = new AccountManager();
-        $manager->decrease($request['id']);
-        $this->redirect('dashboard');
     }
 
     public function deleteAccount($request)
     {
-        if ($this->sessionManager->get('role') !== 'Admin') {
-            $this->redirect('error/message/vous n\'êtes pas Administrateur');
-            exit();
+        if ($this->sessionManager->get('role') === 'Abonné') {
+            $manager = new AccountManager();
+            $account = $manager->find($request['id']);
+            $manager->delete($account);
+            $this->redirect('dashboard');
         }
-
-        $manager = new AccountManager();
-        $account = $manager->find($request['id']);
-        $manager->delete($account);
-        $this->redirect('dashboard');
     }
 }
 
